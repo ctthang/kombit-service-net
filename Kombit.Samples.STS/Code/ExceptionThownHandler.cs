a9e9068d5db3ca8bd205be8483b182c0c332aec8
Le Tran Xuan Phuong <lxp@globeteam.com>
2015-11-26 23:29:29 +0700

    #13596 Re-configure all samples to work with projekt-stoettesystemerne.dk - .Net STS samples

---
+++ Kombit.Samples.STS/Code/ExceptionThownHandler.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.ServiceModel.Channels;
+using System.ServiceModel.Dispatcher;
+using System.Web;
+
+namespace Kombit.Samples.STS.Code
+{
+    public class ExceptionThownHandler : IErrorHandler
+    {
+        public bool HandleError(Exception error)
+        {
+            if (error == null)
+                return false;
+            return true;
+        }
+
+        public void ProvideFault(Exception error, MessageVersion version, ref Message msg)
+        {
+            if (error == null)
+                return;
+
+            var code = "5050";
+            var faultException = error as FaultException;
+            if (faultException != null)
+            {
+                //return;
+                code = faultException.Code.Name;
+            }
+
+            //var customFault = new FaultException(new FaultReason(error.Message),
+            //        new FaultCode("5050"));
+            var customFault = new FaultException(new FaultReason(error.Message),
+                    new FaultCode(code));
+
+            // Create the message fault
+            MessageFault mf = customFault.CreateMessageFault();
+
+            // Update reference to point to the message
+            msg = Message.CreateMessage(version, mf,
+                "http://docs.oasis-open.org/ws-sx/ws-trust/200512/RST/Fault");
+        }
+    }
+}
---
+++ Kombit.Samples.STS/Code/KombitExceptionThrownHandler.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.ServiceModel;
+using System.ServiceModel.Channels;
+using System.ServiceModel.Configuration;
+using System.ServiceModel.Description;
+using System.ServiceModel.Dispatcher;
+using System.Web;
+
+namespace Kombit.Samples.STS.Code
+{
+    public class KombitExceptionThrownHandler : BehaviorExtensionElement, IServiceBehavior
+    {
+        /// <summary>
+        /// Get behavior type
+        /// </summary>
+        public override Type BehaviorType
+        {
+            get { return GetType(); }
+        }
+
+        /// <summary>
+        /// Get behavior
+        /// </summary>
+        /// <returns></returns>
+        protected override object CreateBehavior()
+        {
+            return this;
+        }
+
+        /// <summary>
+        /// Get instance of error handler which is ExceptionhrownHandler in this sample
+        /// </summary>
+        /// <returns></returns>
+        private IErrorHandler GetInstance()
+        {
+            return new ExceptionThownHandler();
+        }
+
+        void IServiceBehavior.AddBindingParameters(ServiceDescription serviceDescription, ServiceHostBase serviceHostBase, Collection<ServiceEndpoint> endpoints, BindingParameterCollection bindingParameters)
+        {
+        }
+
+        /// <summary>
+        /// Apply the ExceptionThrownHandler for service's dispatcher
+        /// </summary>
+        /// <param name="serviceDescription"></param>
+        /// <param name="serviceHostBase"></param>
+        void IServiceBehavior.ApplyDispatchBehavior(ServiceDescription serviceDescription, ServiceHostBase serviceHostBase)
+        {
+            IErrorHandler errorHandlerInstance = GetInstance();
+            foreach (ChannelDispatcher dispatcher in serviceHostBase.ChannelDispatchers)
+            {
+                dispatcher.ErrorHandlers.Add(errorHandlerInstance);
+            }
+        }
+
+        /// <summary>
+        /// Validate service behavior
+        /// </summary>
+        /// <param name="serviceDescription"></param>
+        /// <param name="serviceHostBase"></param>
+        void IServiceBehavior.Validate(ServiceDescription serviceDescription, ServiceHostBase serviceHostBase)
+        {
+            //do no thing
+        }
+    }
+}
